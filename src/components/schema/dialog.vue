6774f21527c69efc2efebe9b1950a72386ef8e34
wuhong <wuh131420@foxmail.com>
2019-02-25 18:37:53 +0800

    2019-02-25-[add][新增dialog组件]

---
+++ src/components/schema/dialog.vue
@@ -0,0 +1,152 @@
+<template>
+  <section class="my-dialog-page">
+    <el-dialog :visible.sync="visibleDialog" :title="title" :before-close="CLEAR_DIALOG_INFO">
+      <el-form
+        :model="formObj.form"
+        ref="myDialogForm"
+        label-width='120px'>
+        <el-form-item
+          v-for="(item, index) in formObj.list"
+          :key="'d' + index"
+          :label="item.label + ' : '"
+          :rules="item.rules"
+          :prop="item.field">
+          <template v-if="item.type === 'default'">
+            <span>{{formObj.form[item.field]}}</span>
+          </template>
+          <template v-if="item.type === 'textarea'">
+            <el-input class="my-input__220" type="textarea" :rows="item.rows" v-model="formObj.form[item.field]" :placeholder="'请编辑' + item.label" />
+          </template>
+          <template v-if="item.type === 'input'">
+            <el-input class="my-input__220" v-model="formObj.form[item.field]" :placeholder="'请编辑' + item.label" />
+          </template>
+          <template v-if="item.type === 'switch'">
+            <el-switch
+              :value="formObj.form[item.field]"
+              :active-text="item.activeText"
+              :inactive-text="item.inactiveText"
+              :active-value="item.activeValue"
+              :inactive-value="item.inactiveValue"
+            ></el-switch>
+          </template>
+          <template v-if="item.type === 'uploadImg'">
+            <el-upload
+              :action="uploadPath"
+              class="avatar-uploader"
+              :show-file-list="false"
+              >
+                <img v-if="formObj.form[item.field]" :src="formObj.form[item.field]">
+                <i v-else class="el-icon-plus avatar-uploader-icon" ></i>
+            </el-upload>
+          </template>
+        </el-form-item>
+      </el-form>
+      <div class="" slot="footer">
+        <el-button type="danger" @click="SET_DIALOG_INFO({isShowDialog: false})">取消</el-button>
+        <el-button type="primary" @click="handleClickSubmit">确定</el-button>
+      </div>
+    </el-dialog>
+  </section>
+</template>
+<script>
+import {mapState, mapActions, mapGetters, mapMutations} from 'vuex'
+import Form from '@/utils/form'
+
+export default {
+  props: {},
+  name: '',
+  components: {},
+  computed: {
+    ...mapState({
+      visibleDialog: state => state.dialogInfo.isShowDialog,
+      title: state => state.dialogInfo.title,
+      temp_form: state => state.dialogInfo.form
+    })
+  },
+  filters: {},
+  data(){
+    return {
+      uploadPath: window.uploadPath,
+      Form,
+      form: {},
+      formObj: {},
+    }
+  },
+  watch: {
+    temp_form(){
+      this.formObj = {
+        ...this.formObj,
+        form: this.temp_form
+      }
+    }
+  },
+  methods: {
+    ...mapMutations(['SET_DIALOG_INFO', 'CLEAR_DIALOG_INFO', 'CLEAR_DIALOG_INFO']),
+    ...mapActions(['UpdateUserByParams', 'ClearDialogInfoAsync']),
+    getFormList(){
+      let {query: {l, f, c}} = this.$route
+      let _obj = Form.find(k => (k.c ? (k.l === l && k.f === f) : (k.l === l && k.f === f && k.c === c)))
+      this.formObj = _obj || {}
+    },
+
+    /**
+     * [handleClickSubmit 表单的提交事件]
+     * @method handleClickSubmit
+     * @return {[type]}          [description]
+     */
+    handleClickSubmit(){
+      this.$refs.myDialogForm.validate(valid => {
+        if(valid){
+          let form = this.formObj.form
+          let {query: {l, f, c}} = this.$route
+          let actions = {
+            '账号管理_账号管理': {
+              action: 'UpdateUserByParams',
+              params: {}
+            }
+          }
+          let action = c ? actions[`${l}_${f}_${c}`] : actions[`${l}_${f}`]
+          this[action.action].call(this, {...form, ...action.params})
+        }else {
+          _g._toast({
+            type: 'error',
+            msg: '请重新编辑错误或必填项目',
+            duration: 3
+          })
+        }
+      })
+    },
+  },
+  created(){
+    this.getFormList()
+  },
+  mixins:[]
+}
+</script>
+<style lang="scss" scoped>
+.my-dialog-page {
+  .my-input__220 {
+    width: 90%;
+  }
+
+  .avatar-uploader {
+  width: 178px;
+  border: 1px dashed #d9d9d9;
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+}
+.avatar-uploader .el-upload:hover {
+  border-color: #409EFF;
+}
+.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 178px;
+  height: 178px;
+  line-height: 178px;
+  text-align: center;
+}
+}
+</style>
